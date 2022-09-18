8e27ce9998126ca38e096c87405925ae2382a125
pranto <polasahammedpranto@gmail.com>
2022-09-18 10:25:49 +0600

    operators

--- datatypes.js
+++ datatypes.js
@@ -5,17 +5,22 @@
     const number = 38;
     const floatNumber = 40.10;
     // Number(), parseFloat(), parseInt(), NaN
+    // Hexadecimal number (16) => const hexadecimal = 0X
+    // Octal number (8) => const octal = 0
 
 // 2. string ( সকল ধরনের text )
     const string1 = 'string';
     const string2 = "string";
     const string3 = `string`;
-    // string() = conver to string
+    // String() = conver to string, toString(),
 
 // 3. boolean
+    // Boolean()
     // true, false
 // 4. undefind
 // 5. null
+
+// Falsy Values ("", 0, undefined null, NaN)
 
 
 // object ডাটা টাইপ যেটা আমরা নিজেরা তৈরি করব জাভাস্ক্রিপ্ট keyword এর মাধ্যমে
---
+++ operators.js
@@ -0,0 +1,61 @@
+// Arithmetic operators
+// +, -, *, /, %, ++, --
+
+// ভাগশেষ বের করার জন্য ব্যবহৃত হয় ( % );
+// let num1 = 10;
+// let num2 = 3;
+// console.log(num1 % num2);
+
+// incremental
+// 1. pre incremental ( num1++ ) => num1 = num1 + 1;
+// 2. post incremental ( ++num1 )
+
+
+// Assignment Oterators ( = )
+// let number1 = 20;
+// let number2 = 10;
+// number1 += number2;
+// number1 -= number2;
+// number1 *= number2;
+// number1 /= number2;
+// number1 %= number2;
+// console.log(number1);
+
+
+// Comparsion operators ( ==, ===, !=, >, <, >=, <=)
+// Comparsion operator দুইটা জিনিস return করে একটি হচ্ছে হচ্ছে true আরেকটা হচ্ছে false
+
+// let number1 = 20;
+// let number2 = 10;
+// console.log(number1 == number2);
+// console.log(number1 != number2);
+// console.log(number1 > number2);
+// console.log(number1 < number2);
+// console.log(number1 >= number2);
+// console.log(number1 <= number2);
+
+
+// logical operators ( &&, ||, !)
+
+// && => দুইটা কন্ডিশন true হতে হবে এবং সেটা রিটার্ন করবে
+// || => যেকোনো একটা true হলেই সে true রিটার্ন করবে
+
+// const number1 = 40;
+// const number2 = 20;
+// console.log(number1 > number2 && number1 < number2); (false)
+// console.log(number1 > number2 || number1 < number2); (true)
+
+// Bitwise operators
+// &
+// |
+// ~
+// ^
+// >>
+// <<
+
+
+// type of operators ( ডাটা টাইপ টা কি ধরনের )
+
+// console.log(typeof 10); (number)
+// console.log(typeof "pranto"); (string)
+
