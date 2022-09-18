36904a99d3994c0ef32f74c13378af263820c9e6
pranto <polasahammedpranto@gmail.com>
2022-09-18 09:05:18 +0600

    data type

---
+++ datatypes.js
@@ -0,0 +1,25 @@
+// জাভাস্ক্রিপ্টে দুই ধরনের ডাটা টাইপ রয়েছে। একটি হচ্ছে primitive  ডাটা টাইপ আরেকটি  হচ্ছে object।
+// primitive ডাটা টাইপ এমন এক ডাটা টাইপ যেটা জাভাস্ক্রিপ্ট দিয়ে দেয় উদাহরণস্বরূপ (number, string, boolean, undefined, null)
+
+// 1. number ( সকল ধরনের নাম্বার )
+    const number = 38;
+    const floatNumber = 40.10;
+    // Number(), parseFloat(), parseInt(), NaN
+
+// 2. string ( সকল ধরনের text )
+    const string1 = 'string';
+    const string2 = "string";
+    const string3 = `string`;
+    // string() = conver to string
+
+// 3. boolean
+    // true, false
+// 4. undefind
+// 5. null
+
+
+// object ডাটা টাইপ যেটা আমরা নিজেরা তৈরি করব জাভাস্ক্রিপ্ট keyword এর মাধ্যমে
+
+// 1.Array
+// 2.object
+// 3.function
