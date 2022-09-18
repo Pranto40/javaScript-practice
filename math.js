7787463271664105388446cc4f299be39a6544df
pranto <polasahammedpranto@gmail.com>
2022-09-18 10:51:22 +0600

    math

---
+++ math.js
@@ -0,0 +1,15 @@
+// console.log(Math.E); (2.718281828459045)
+// console.log(Math.PI); (3.141592653589793)
+let number = 52.38
+// console.log(Math.abs(number)); (52.38)
+// console.log(Math.floor(number)); (52)
+// console.log(Math.ceil(number)); (53)
+// console.log(Math.round(number)); (52)
+// console.log(Math.max(500, 300)); (500)
+// console.log(Math.min(400, 200)); (200)
+// console.log(Math.pow(2, 3)); ==> 2 power 3 => 8
+// console.log(Math.sqrt(9)); (3)
+
+// random number ( 0 থেকে 1 এর মধ্যে )
+// আমরা 1 থেকে 20 এর মধ্যে যেকোনো একটি নাম্বার জেনারেট করতে যাচ্ছি ( * 20 + 1 )
+// console.log(Math.round(Math.random() * 20 + 1));
