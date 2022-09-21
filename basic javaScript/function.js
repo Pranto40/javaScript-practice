e07bfa41964e167f00769970dd2b8833d4f2d955
pranto <polasahammedpranto@gmail.com>
2022-09-21 09:17:32 +0600

    basic javaScript

---
+++ basic javaScript/function.js
@@ -0,0 +1,28 @@
+// একই ধরনের কাজ বারবার করা লাগছে সে ক্ষেত্রে আমরা ফাংশন ইউজ করব
+
+// const numbers = [ 20, 10, 50,32, 12, 21, 52, 40]
+
+// function addArray(numbers) {
+//     let sum = 0
+//     for (let i = 0; i < numbers.length; i++) {
+//         const element = numbers[i];
+//         sum += element
+//     }
+//     console.log(sum);
+// }
+
+// addArray(numbers)
+
+// ফাংশন expression এটা হচ্ছে কোন একটা function কে variable  রাখা।
+// const expressionFuntion = function () {
+    
+// }
+
+// একটা ফাংশন এর ভিতরে আরেকটা ফাংশন create করা কে inner ফাংশন বলে
+// function somthing() {
+//     function sayHi() {
+//         console.log("hello! I am pranto");
+//     }
+//     sayHi()
+// }
+// somthing()
