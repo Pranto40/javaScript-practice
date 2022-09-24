d58eee84e0e335519760aad91ed5789e5efaadae
pranto <polasahammedpranto@gmail.com>
2022-09-24 08:29:44 +0600

    javascript engine

---
+++ JavaScript Engine/javaScriptEngineWork.js
@@ -0,0 +1,8 @@
+// জাভাস্ক্রিপ্ট ইঞ্জিন হচ্ছে একটি প্রোগ্রাম। জাভাস্ক্রিপ্ট ইঞ্জিন তিনটা কাজ থাকে read, understand and translate। আমি যে source code  লিখছি সেটা কে read করবে বুঝার চেষ্টা করবে এবং সেটাকে translate  করে মেশিনে রূপান্তর করবে।
+
+// javaScript Engine => v8 > google, spider monkey > firefox, chakar > microsoft, javascript code > app safari
+
+// source code > parser > abstract window tree > mechin code > execution
+
+// source code :- আমরা জাভাস্ক্রিপ্টে যে ধরনের code লেখি সেটাই হচ্ছে সোর্স কোড
+// parser :- জাভাস্ক্রিপ্ট কম্পাইলার। প্রত্যেকটা লাইন সে read করবে এবং read করার সময় একটি ডাটা স্ট্রাকচার তৈরি করবে সেটা হচ্ছে abstract window tree
